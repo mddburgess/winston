7ce8de8bb08243af737e024f2ec37f1c9f5e27e7
Mike Burgess <mddburgess@users.noreply.github.com>
2025-02-26 12:01:21 -0800

    Add react-bootstrap dependency.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router";
 import {App} from "./App";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = document.getElementById("root")!;
 createRoot(root).render(
