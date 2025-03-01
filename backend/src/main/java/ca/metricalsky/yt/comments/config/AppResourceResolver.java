51912f5ea30f2c3e80d7c38d3b64715e5be29579
Mike Burgess <mddburgess@users.noreply.github.com>
2025-03-01 12:16:39 -0800

    Don't return classpath resource for invalid API endpoints.

--- backend/src/main/java/ca/metricalsky/yt/comments/config/AppResourceResolver.java
+++ backend/src/main/java/ca/metricalsky/yt/comments/config/AppResourceResolver.java
@@ -3,20 +3,21 @@ package ca.metricalsky.yt.comments.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
-
-import static com.google.common.base.MoreObjects.firstNonNull;
 
 @Configuration
 public class AppResourceResolver extends PathResourceResolver {
 
     @Override
+    @Nullable
     protected Resource getResource(String resourcePath, Resource location) throws IOException {
-        return firstNonNull(
-                super.getResource(resourcePath, location),
-                new ClassPathResource("/static/index.html")
-        );
+        var resource = super.getResource(resourcePath, location);
+        if (resource == null && !resourcePath.startsWith("api")) {
+            resource = new ClassPathResource("/static/index.html");
+        }
+        return resource;
     }
 }
