897a66f17351cf494d294a6f7ee618aa1f2eb6ec
Mike Burgess <mddburgess@users.noreply.github.com>
2025-03-02 10:58:21 -0800

    Add code coverage report to build.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -52,7 +53,12 @@ tasks {
         ))
     }
 
-    withType<Test> {
+    test {
         useJUnitPlatform()
+        finalizedBy(jacocoTestReport)
+    }
+
+    jacocoTestReport {
+        dependsOn(test)
     }
 }
