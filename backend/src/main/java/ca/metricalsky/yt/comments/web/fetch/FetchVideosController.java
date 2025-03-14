a513d53d9da684559a9ccf7fdf5401796557ad24
Mike Burgess <mddburgess@users.noreply.github.com>
2025-03-14 10:35:59 -0700

    Make notify subscription header optional for testing.

--- backend/src/main/java/ca/metricalsky/yt/comments/web/fetch/FetchVideosController.java
+++ backend/src/main/java/ca/metricalsky/yt/comments/web/fetch/FetchVideosController.java
@@ -4,12 +4,16 @@ import ca.metricalsky.yt.comments.service.ChannelService;
 import ca.metricalsky.yt.comments.service.NotificationsService;
 import ca.metricalsky.yt.comments.service.fetch.FetchVideosService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @RestController
@@ -21,13 +25,19 @@ public class FetchVideosController {
     private final NotificationsService notificationsService;
 
     @PostMapping("/api/channels/{channelId}/fetch-videos")
-    public ResponseEntity<Void> fetchVideosForChannel(
+    public ResponseEntity<SseEmitter> fetchVideosForChannel(
             @PathVariable String channelId,
-            @RequestHeader("X-Notify-Subscription") UUID subscriptionId
-    ) {
+            @RequestHeader(value = "X-Notify-Subscription", required = false) UUID subscriptionId
+    ) throws IOException {
         channelService.requireChannelExists(channelId);
-        var sseEmitter = notificationsService.requireSubscription(subscriptionId);
+        var sseEmitter = subscriptionId != null
+                ? notificationsService.requireSubscription(subscriptionId)
+                : notificationsService.openSubscription();
+
         fetchVideosService.fetchVideosForChannel(channelId, sseEmitter);
-        return ResponseEntity.accepted().build();
+
+        return subscriptionId != null
+                ? ResponseEntity.accepted().build()
+                : ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_EVENT_STREAM).body(sseEmitter);
     }
 }
