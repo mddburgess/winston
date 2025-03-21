d41e226dd7f8163e2103e0601c493e8179c482ff
Mike Burgess <mddburgess@users.noreply.github.com>
2025-03-20 18:31:54 -0700

    Add button to fetch channel.

--- frontend/src/routes/channels/index.tsx
+++ frontend/src/routes/channels/index.tsx
@@ -1,12 +1,26 @@
 import {useListChannelsQuery} from "../../store/slices/api";
 import {ChannelCards} from "./ChannelCards";
+import {Button, Col, Row, Stack} from "react-bootstrap";
+import {ArrowDownRightCircleFill} from "react-bootstrap-icons";
 
 export const ChannelsRoute = () => {
     const { data: channels } = useListChannelsQuery()
 
     return (
         <>
-            <h1 className={"mb-2"}>Channels</h1>
+            <Row className={"mb-2"}>
+                <Col className={"align-items-center d-flex"}>
+                    <p className={"h1 m-0"}>
+                        Channels
+                    </p>
+                </Col>
+                <Col xs={"auto"} className={"align-items-center d-flex"}>
+                    <Button className={"align-items-center d-flex"}>
+                        Fetch...
+                        <ArrowDownRightCircleFill className={"ms-2"}/>
+                    </Button>
+                </Col>
+            </Row>
             <ChannelCards channels={channels} />
         </>
     )
