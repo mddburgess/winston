eb3d43169ab7648f8b6daa18af4e8196b22ce5a7
Mike Burgess <mddburgess@users.noreply.github.com>
2025-03-26 17:48:05 -0700

    Visual adjustments to channel details page.

---
+++ frontend/src/components/BadgeList.tsx
@@ -0,0 +1,25 @@
+import {Badge} from "react-bootstrap";
+
+type BadgeListProps = {
+    values: string[],
+    transformer?: (value: string) => string
+}
+
+const identity = (value: string) => value;
+
+export const BadgeList = ({values, transformer = identity}: BadgeListProps) => {
+    const badges = values.map(transformer).map(value =>
+        <Badge
+            key={value} pill
+            className={"bg-secondary-subtle text-secondary-emphasis border border-secondary-subtle me-2 mb-2"}
+        >
+            {value}
+        </Badge>
+    )
+
+    return (
+        <>
+            {...badges}
+        </>
+    )
+}
--- frontend/src/routes/channels/id/ChannelDetails.tsx
+++ frontend/src/routes/channels/id/ChannelDetails.tsx
@@ -1,18 +1,53 @@
 import {ChannelDto} from "../../../model/ChannelDto";
 import {Col, Image, Row} from "react-bootstrap";
+import {Clipboard} from "react-bootstrap-icons";
+import {BadgeList} from "../../../components/BadgeList";
 
 type ChannelDetailsProps = {
     channel: ChannelDto
 }
 
 export const ChannelDetails = ({channel}: ChannelDetailsProps) => (
-    <Row>
+    <Row className={"bg-body-tertiary border mx-0 my-3 p-3 rounded-3"}>
         <Col xs={"auto"}>
-            <Image width={120} height={120} src={channel.thumbnailUrl}/>
+            <Image
+                roundedCircle
+                className={"border"}
+                width={120} height={120}
+                src={channel.thumbnailUrl}
+            />
         </Col>
         <Col>
-            <h2>{channel.title}</h2>
-            <h3>{channel.customUrl}</h3>
+            <p className={"h1"}>
+                {channel.title}
+            </p>
+            <p className={"align-items-center d-flex h6"}>
+                <a href={`https://www.youtube.com/${channel.customUrl}`} target={"_blank"}>
+                    {channel.customUrl}
+                </a>
+                <Clipboard className={"ms-2"}/>
+            </p>
+            <p className={"small"}>
+                {channel.description}
+            </p>
+            <Row xs={1} lg={2}>
+                <Col className={"mb-3 mb-lg-0"}>
+                    <p className={"h6"}>
+                        Topics
+                    </p>
+                    <BadgeList values={channel.topics} transformer={topicTransformer}/>
+                </Col>
+                <Col>
+                    <p className={"h6"}>
+                        Keywords
+                    </p>
+                    <BadgeList values={channel.keywords}/>
+                </Col>
+            </Row>
         </Col>
     </Row>
 )
+
+const topicTransformer = (topic: string) => {
+    return topic.slice("https://en.wikipedia.org/wiki/".length).replace(/_/g, " ");
+}
