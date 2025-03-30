3d6e9a0c9eb35219f6ebe1497cfbd470328f6ba6
Mike Burgess <mddburgess@users.noreply.github.com>
2025-03-30 14:58:22 -0700

    Render comments as HTML.

---
+++ frontend/src/components/HtmlText.tsx
@@ -0,0 +1,9 @@
+import {sanitize} from "dompurify";
+
+type HtmlTextProps = {
+    text: string,
+}
+
+export const HtmlText = ({text}: HtmlTextProps) => (
+    <span dangerouslySetInnerHTML={{ __html: sanitize(text) }}/>
+);
