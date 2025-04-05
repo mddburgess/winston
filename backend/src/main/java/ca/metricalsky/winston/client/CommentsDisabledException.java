619929bc4d27d540c8451f60774b2304ca8a2d15
Mike Burgess <mddburgess@users.noreply.github.com>
2025-04-05 08:47:40 -0700

    Show on video page when comments are disabled.

--- backend/src/main/java/ca/metricalsky/winston/client/CommentsDisabledException.java
+++ backend/src/main/java/ca/metricalsky/winston/client/CommentsDisabledException.java
@@ -3,11 +3,15 @@ package ca.metricalsky.winston.client;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import org.springframework.http.HttpStatus;
 
+import java.net.URI;
+
 public class CommentsDisabledException extends YouTubeException {
 
+    private static final URI TYPE = URI.create("/api/problem/comments-disabled");
     private static final String MESSAGE = "Comments are disabled for the requested video.";
 
     CommentsDisabledException(GoogleJsonResponseException cause) {
         super(HttpStatus.UNPROCESSABLE_ENTITY, MESSAGE, cause);
+        getBody().setType(TYPE);
     }
 }
--- frontend/src/model/events/FetchEvent.ts
+++ frontend/src/model/events/FetchEvent.ts
@@ -1,12 +1,24 @@
 import {ChannelDto} from "../ChannelDto";
 import {VideoWithChannelIdDto} from "../VideoDto";
 import {CommentDto} from "../CommentDto";
+import {ProblemDetail} from "./ProblemDetail";
 
-export type FetchEvent<T> = {
-    objectId: string;
+
+type FetchDataEvent<T> = {
     status: 'FETCHING' | 'COMPLETED';
     items: T[];
+    error: undefined;
 }
+
+type FetchErrorEvent = {
+    status: 'FAILED';
+    items: undefined;
+    error: ProblemDetail;
+}
+
+type FetchEvent<T> = {
+    objectId: string;
+} & (FetchDataEvent<T> | FetchErrorEvent);
 
 export type FetchChannelEvent = FetchEvent<ChannelDto>;
 export type FetchVideosEvent = FetchEvent<VideoWithChannelIdDto>;
