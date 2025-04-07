afe1c1daada9b94ee16e9059b0889a65753d6735
Mike Burgess <mddburgess@users.noreply.github.com>
2025-04-06 18:19:34 -0700

    Report frontend test results to Codecov.

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -109,7 +109,10 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+      [ "default", { } ],
+      [ "jest-junit", { outputDirectory: "coverage" } ],
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
