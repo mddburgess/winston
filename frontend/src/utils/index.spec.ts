66e1cab1738bee8cffe14e3ab310e1140ccf4415
Mike Burgess <mddburgess@users.noreply.github.com>
2025-04-06 17:50:10 -0700

    Add frontend code coverage.

---
+++ frontend/src/utils/index.spec.ts
@@ -0,0 +1,24 @@
+import {pluralize} from "./index";
+
+describe("pluralize", () => {
+
+    it("returns plural value when count is 0", () => {
+        expect(pluralize(0, "channel"))
+            .toBe("0 channels");
+    })
+
+    it("returns singular value when count is 1", () => {
+        expect(pluralize(1, "channel"))
+            .toBe("1 channel");
+    })
+
+    it("returns plural value when count is greater than 1", () => {
+        expect(pluralize(2, "channel"))
+            .toBe("2 channels");
+    })
+
+    it("returns custom plural value when it is specified", () => {
+        expect(pluralize(2, "reply", "replies"))
+            .toBe("2 replies");
+    })
+})
