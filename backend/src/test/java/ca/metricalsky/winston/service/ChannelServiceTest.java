965bd48fffce647e4e0faa720cfe272e31afff1b
Mike Burgess <mddburgess@users.noreply.github.com>
2025-05-02 17:38:06 -0700

    Add unit tests.

--- backend/src/test/java/ca/metricalsky/winston/service/ChannelServiceTest.java
+++ backend/src/test/java/ca/metricalsky/winston/service/ChannelServiceTest.java
@@ -10,6 +10,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,6 +32,36 @@ class ChannelServiceTest {
     private ChannelRepository channelRepository;
     @Mock
     private VideoService videoService;
+
+    @Test
+    void findAll() {
+        when(videoService.countAllByChannelId())
+                .thenReturn(Map.of(CHANNEL_ID, 1L));
+        when(channelRepository.findAll())
+                .thenReturn(List.of(buildChannel()));
+
+        var channelDtos = channelService.findAll();
+
+        assertThat(channelDtos)
+                .hasSize(1)
+                .first()
+                .hasFieldOrPropertyWithValue("id", CHANNEL_ID)
+                .hasFieldOrPropertyWithValue("customUrl", CHANNEL_HANDLE)
+                .hasFieldOrPropertyWithValue("videoCount", 1L);
+    }
+
+    @Test
+    void findAll_empty() {
+        when(videoService.countAllByChannelId())
+                .thenReturn(Map.of());
+        when(channelRepository.findAll())
+                .thenReturn(List.of());
+
+        var channelDtos = channelService.findAll();
+
+        assertThat(channelDtos)
+                .isEmpty();
+    }
 
     @Test
     void findByHandle() {
