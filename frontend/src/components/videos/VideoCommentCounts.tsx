f2fc3de8fd7bd6c9b3bf78666b4466b5d0c3c2e0
Mike Burgess <mddburgess@users.noreply.github.com>
2025-09-13 10:00:42 -0700

    Video details comment count includes the reply count.

--- frontend/src/components/videos/VideoCommentCounts.tsx
+++ frontend/src/components/videos/VideoCommentCounts.tsx
@@ -5,15 +5,21 @@ type VideoCommentCountsProps = VideoProps & {
   showTotalReplyCount?: boolean;
 };
 
-const VideoCommentCounts = ({ video, showTotalReplyCount }: VideoCommentCountsProps) => (
-  <CommentCounts
-    commentsDisabled={video.comments?.comments_disabled}
-    commentCount={Math.max(video.details?.comment_count ?? 0, video.comments?.comment_count ?? 0)}
-    replyCount={video.comments?.reply_count}
-    totalReplyCount={video.comments?.total_reply_count}
-    lastFetchedAt={video.comments?.last_fetched_at}
-    showTotalReplyCount={showTotalReplyCount}
-  />
-);
+const VideoCommentCounts = ({ video, showTotalReplyCount }: VideoCommentCountsProps) => {
+  const detailsCommentAndReplyCount = video.details?.comment_count ?? 0;
+  const totalReplyCount = video.comments?.total_reply_count ?? 0;
+  const detailsCommentCount = detailsCommentAndReplyCount - totalReplyCount;
+
+  return (
+    <CommentCounts
+      commentsDisabled={video.comments?.comments_disabled}
+      commentCount={Math.max(detailsCommentCount, video.comments?.comment_count ?? 0)}
+      replyCount={video.comments?.reply_count}
+      totalReplyCount={video.comments?.total_reply_count}
+      lastFetchedAt={video.comments?.last_fetched_at}
+      showTotalReplyCount={showTotalReplyCount}
+    />
+  );
+};
 
 export { VideoCommentCounts };
