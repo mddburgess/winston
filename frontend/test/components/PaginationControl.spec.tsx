2129df94b8e885dc2195d2abf66eba41240f63e9
Mike Burgess <mddburgess@users.noreply.github.com>
2025-10-01 18:05:31 -0700

    Add unit tests.

--- frontend/test/components/PaginationControl.spec.tsx
+++ frontend/test/components/PaginationControl.spec.tsx
@@ -1,14 +1,14 @@
 import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router";
+import { MemoryRouter } from "react-router";
 import { describe, expect, it } from "vitest";
 import { PaginationControl } from "#/components/PaginationControl";
 
 describe(PaginationControl, () => {
   it("is not displayed when the total count is 0", () => {
     const result = render(
-      <BrowserRouter>
+      <MemoryRouter>
         <PaginationControl totalCount={0} pageSize={10} pageNumber={0} />
-      </BrowserRouter>,
+      </MemoryRouter>,
     );
     const buttons = result.queryAllByRole("button");
 
@@ -17,12 +17,23 @@ describe(PaginationControl, () => {
 
   it("is not displayed when the total count is equal to the page size", () => {
     const result = render(
-      <BrowserRouter>
+      <MemoryRouter>
         <PaginationControl totalCount={10} pageSize={10} pageNumber={0} />
-      </BrowserRouter>,
+      </MemoryRouter>,
     );
     const buttons = result.queryAllByRole("button");
 
     expect(buttons).toHaveLength(0);
+  });
+
+  it("is displayed when the total count is greater than the page size", () => {
+    const result = render(
+      <MemoryRouter>
+        <PaginationControl totalCount={30} pageSize={10} pageNumber={1} />
+      </MemoryRouter>,
+    );
+    const buttons = result.getAllByRole("button");
+
+    expect(buttons).toHaveLength(4);
   });
 });
