6c19a6102082d8204866c3e809309ecd04c92adb
Mike Burgess <mddburgess@users.noreply.github.com>
2026-04-26 18:44:05 -0700

    Move video detail entities.

---
+++ database/src/main/kotlin/ca/metricalsky/winston/database/entity/video/VideoRestrictionEntity.kt
@@ -0,0 +1,36 @@
+package ca.metricalsky.winston.database.entity.video
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.sql.Restriction
+
+@Entity
+@Table(name = "video_restrictions")
+class VideoRestrictionEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long,
+
+    @Column(name = "video_id")
+    var videoId: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "restriction")
+    var restriction: Restriction,
+
+    @Column(name = "country")
+    var country: String
+) {
+    enum class Restriction {
+        ALLOWED,
+        BLOCKED
+    }
+}
