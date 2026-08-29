059a81cad7758ed504131b3c849be7858fd6b9ca
Mike Burgess <mddburgess@users.noreply.github.com>
2026-08-28 17:31:33 -0700

    Add constraint violation exception handler.

--- backend/src/main/kotlin/ca/metricalsky/winston/exception/handlers/ConstraintViolationExceptionHandler.kt
+++ backend/src/main/kotlin/ca/metricalsky/winston/exception/handlers/ConstraintViolationExceptionHandler.kt
@@ -6,10 +6,24 @@ import ca.metricalsky.winston.api.model.ProblemLocation
 import ca.metricalsky.winston.exception.ErrorCode
 import jakarta.validation.ConstraintViolation
 import jakarta.validation.ConstraintViolationException
+import jakarta.validation.ElementKind
+import jakarta.validation.Path
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import org.hibernate.validator.internal.metadata.descriptor.ConstraintDescriptorImpl
+import org.springframework.context.MessageSource
 import org.springframework.stereotype.Component
+import java.util.Locale
 
 @Component
-class ConstraintViolationExceptionHandler: ExceptionHandler<ConstraintViolationException> {
+class ConstraintViolationExceptionHandler(
+    private val messageSource: MessageSource
+): ExceptionHandler<ConstraintViolationException> {
+
+    private val types = mapOf(
+        NotNull::class.java to "missing-required-property",
+        Size::class.java to "invalid-property-length",
+    )
 
     override fun handleException(exception: ConstraintViolationException): Problem {
         val errors = exception.constraintViolations.map { buildError(it) }
@@ -17,9 +31,26 @@ class ConstraintViolationExceptionHandler: ExceptionHandler<ConstraintViolationE
     }
 
     private fun buildError(violation: ConstraintViolation<*>): ProblemError {
+        val type = when (val descriptor = violation.constraintDescriptor) {
+            is ConstraintDescriptorImpl -> types[descriptor.annotationType]
+                ?: "constraint:${descriptor.annotationType.simpleName}"
+            else -> violation.constraintDescriptor.messageTemplate
+        }
+        val message = try {
+            messageSource.getMessage(type, null, Locale.ENGLISH)
+        } catch (e: Exception) {
+            violation.message
+        }
+
         return ProblemError()
-            .type("error:${violation.messageTemplate}")
-            .detail(violation.message)
-            .location(ProblemLocation().pointer(violation.propertyPath.toString()))
+            .type("error:${type}")
+            .detail(message)
+            .location(ProblemLocation().pointer(violation.propertyPath.toJsonPointer()))
     }
+
+    private fun Path.toJsonPointer() =
+        this.filter { it.kind == ElementKind.PROPERTY }
+            .flatMap { listOf(it.index, it.name) }
+            .filterNotNull()
+            .joinToString(prefix = "/", separator = "/") { it.toString() }
 }
